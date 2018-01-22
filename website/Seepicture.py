eccafedc34a7560dfa98fcda85f378984af46c60
fancyblock <xujia1985@gmail.com>
2018-01-23 00:51:18 +0800

    hybrid

--- website/Seepicture.py
+++ website/Seepicture.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -19,5 +20,11 @@ def query_result():
     return render_template("query_result.html")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
 if __name__ == "__main__":
+    bootstrap = Bootstrap(app)
     app.run("0.0.0.0", 80, True)
