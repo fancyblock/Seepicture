bb792591139a5905690dcf8dd882e0e31233daa8
hejiabin <jiabin.he@coconutislandstudio.com>
2018-01-23 19:56:50 +0800

    hybrid

--- website/Seepicture.py
+++ website/Seepicture.py
@@ -5,13 +5,17 @@ from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "the password"
+bootstrap = Bootstrap(app)
 
 
+# 主页
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
+# 搜索页面
 @app.route("/query_result", methods=["post"])
 def query_result():
     keyword = request.form["keyword"]
@@ -20,11 +24,11 @@ def query_result():
     return render_template("query_result.html")
 
 
+# 错误页面
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
-    bootstrap = Bootstrap(app)
-    app.run("0.0.0.0", 80, True)
+    app.run("0.0.0.0", 80)
